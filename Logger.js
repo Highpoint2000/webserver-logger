182448e8154ba11da858e638cb0a762e23466dd8
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-06-03 19:49:18 +0200

    Add files via upload

---
+++ Logger.js
@@ -0,0 +1,14 @@
+// Plugin configuration, this is used in the administration when plugins are loaded
+var pluginConfig = {
+    name: 'Logger Plugin',
+    version: '1.0',
+    author: 'Highpoint',
+    frontEndPath: 'Logger/logger-plugin.js'
+}
+
+// Backend (server) changes can go here...
+
+// Don't change anything below here if you are making your own plugin
+module.exports = {
+    pluginConfig
+}
