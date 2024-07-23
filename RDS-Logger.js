65c0f58e40851cab844b0b6c515743e5678ea445
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-07-23 13:29:08 +0200

    Update RDS-Logger.js

--- RDS-Logger.js
+++ RDS-Logger.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'RDS-Logger Plugin',
-    version: '1.3g',
+    version: '1.3h',
     author: 'Highpoint',
     frontEndPath: 'Logger/logger-plugin.js'
 }
